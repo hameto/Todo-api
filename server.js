5623c06f4c65a50c1e3859ccf857d35c1b1dbf29
Hameto <ham3to@gmail.com>
2017-10-08 12:21:10 -0400

    init to-do app

--- server.js
+++ server.js
@@ -2,25 +2,47 @@ var express = require ('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [{
-	id: 01,
+	id: 1,
 	description: 'Meet Mama for lunch',
 	completed: false,
 }, {
 	id: 2,
 	description: 'Go for a bikeride',
 	completed: false,
+}, {
+	id: 3,
+	description: 'Mock up new map idea',
+	completed: true,
 }];
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
 // GET /todos
-app.get('/todos', function(req, res) {
+app.get('/todos', function (req, res) {
 	res.json(todos);
 })
 
 // GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoID = parseInt(req.params.id, 10);
+	var matchedToDo;
+
+	todos.forEach(function (todo) {
+		if (todoID === todo.id) {
+			matchedToDo = todo;
+		} 
+	});
+
+	if (matchedToDo) {
+		res.json(matchedToDo);
+	} else {
+		res.send('404: something is fucked up. try again homie');
+		res.status(404).send();
+	};
+});
+
 
 app.listen(PORT, function() {
 	console.log('Express listening on port ' + PORT + '!');
