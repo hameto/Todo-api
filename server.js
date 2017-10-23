439d99b25417fb62b55ed7b5639979c5da52e170
Hameto <ham3to@gmail.com>
2017-10-22 21:33:16 -0400

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -16,30 +16,28 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=true&description=buy
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(todos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(todos, {
-			completed: false
-		})
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
 	};
 
-	// write query parameter for description that enables user to search for a term w/n the string
-	// use _.filter( , )
-	// use _.indexOf( , )
-
-	if (queryParams.hasOwnProperty('description') && queryParams.description.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.description.toLowerCase()) > -1;
-		});
+	if (query.hasOwnProperty('description') && query.description.length > 0) {
+		where.description = {
+			$like: '%' + query.description + '%'
+		}
 	}
 
-	res.json(filteredTodos);
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
+		res.json(todos);
+	}, function(e) {
+		res.status(500).send();
+	})
 });
 
 // GET /todos/:id
